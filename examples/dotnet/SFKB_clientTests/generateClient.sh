6461bb105f2f6f6e3814e89e92a9f57f3a658acf
jarle pedersen <jarle.pedersen@norconsult.com>
2019-01-30 11:12:25 +0100

    remove redundant installations

--- examples/dotnet/SFKB_clientTests/generateClient.sh
+++ examples/dotnet/SFKB_clientTests/generateClient.sh
@@ -1,4 +1,3 @@
-nvm install node
 npm install -g nswag yamljs
 yaml2json -s ../../../spec/openapi.yaml
 nswag swagger2csclient /input:../../../spec/openapi.json /classname:Client /namespace:SFKB_API /output:Client.cs
