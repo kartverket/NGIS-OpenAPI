45a623711c950723b50054d1d0074c4cd8ee9b3a
Jarle Pedersen <jarle.pedersen@norconsult.com>
2019-01-30 15:29:04 +0100

    Travis-yaml  (#18)
    * travis for generating c# example-code
    
    * use correct version for development
    
    * install dotnet
    
    * try manual installation of node
    
    * move testing to local repo
    
    * update travis
    
    * remove git-commands
    
    * use env-variables
    
    * corrected path
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * update datasetId to new example
    
    * corrected Assert-text
    
    * commented out test that will always fail
    
    * remove static client-code
    
    * moved Client-generation to batch-files
    
    * use sh to run
    
    * remove redundant installations
    
    * release build artifacts

---
+++ examples/dotnet/SFKB_clientTests/generateClient.bat
@@ -0,0 +1,1 @@
+npm install -g nswag yamljs && yaml2json -s ../../../spec/openapi.yaml && nswag swagger2csclient /input:../../../spec/openapi.json /classname:Client /namespace:SFKB_API /output:Client.cs
