e87bc2b6a829268d0d98b37ed3cf3bf2b7b112cd
Jarle Pedersen <Jarle.Pedersen@geodata.no>
2019-12-02 11:33:12 +0100

    added configfile test replace test locking preliminary testing of bbox (commented out)

--- examples/dotnet/SFKB_clientTests/SFKB_clientTest.cs
+++ examples/dotnet/SFKB_clientTests/SFKB_clientTest.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
 using SFKB_API;
 
 namespace SFKB_clientTests
@@ -10,51 +14,182 @@ namespace SFKB_clientTests
     [TestClass]
     public class SfkbClientTest
     {
+        private const string ConfigLocation = @"../../../config.json";
+        private const string ExampleFeaturePath = @"../../../exampleData/exampleFeature.xml";
         private static Client Client;
-        private static readonly Guid DatasetId = new Guid("0b88534d-c975-4b15-a8f3-da16a2101e29");
+        private Guid DatasetId;
         private static readonly Guid WrongDatasetId = new Guid("2fa85f64-5717-4562-b3fc-2c963f66afa6");
+        private const string Ar5DatasetName = "ar5_test_23";
+        private const string Ar5FeatureLokalId = "0003f094-b524-4a5a-bb05-d69881df853a";
 
         [TestInitialize]
-        public void Init() {
-            var username = Environment.GetEnvironmentVariable("api_user");
-            var password = Environment.GetEnvironmentVariable("api_pass");
+        public void Init()
+        {
+            var basicIdentification = File.Exists(ConfigLocation)
+                ? GetIdentificationFromConfigFile()
+                : GetIdentificationFromEnvironmentVariables();
 
-            var byteArray = Encoding.ASCII.GetBytes($"{username}:{password}");
+            var byteArray = Encoding.ASCII.GetBytes(basicIdentification);
 
             var httpClient = new HttpClient();
 
             var basicValue = Convert.ToBase64String(byteArray);
-            
+
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", basicValue);
 
             Client = new Client(httpClient);
+
+            GetDatasets();
+        }
+
+        private static string GetIdentificationFromEnvironmentVariables()
+        {
+            return $"{Environment.GetEnvironmentVariable("api_user")}:{Environment.GetEnvironmentVariable("api_pass")}";
+        }
+
+        private static string GetIdentificationFromConfigFile()
+        {
+            string basicIdentification;
+            var config = JObject.Parse(File.ReadAllText(ConfigLocation));
+
+            basicIdentification = $"{config["user"].ToString()}:{config["password"].ToString()}";
+            return basicIdentification;
         }
 
         [TestMethod]
         public void TestGetDatasets()
         {
+            Assert.IsTrue(GetDataset().Id == DatasetId, $"No dataset found with id {DatasetId}");
+        }
+
+        private Dataset GetDataset()
+        {
+            return Client.GetDatasetMetadataAsync(DatasetId).Result;
+        }
+
+        private void GetDatasets()
+        {
             var datasets = Client.GetDatasetsAsync().Result;
+
             Assert.IsTrue(datasets.Count > 0, "No datasets returned");
+
+            DatasetId = datasets.FirstOrDefault( d => d.Name == Ar5DatasetName).Id;
         }
 
         [TestMethod]
-        public void TestGetDataset()
+        public void TestGetDatasetWithWrongId()
         {
-            var dataset = Client.GetDatasetMetadataAsync(DatasetId).Result;
-            Assert.IsTrue(dataset.Id == DatasetId, $"No dataset found with id {DatasetId}");
+            Exception exception = null;
+            try
+            {
+                var result = Client.GetDatasetMetadataAsync(WrongDatasetId).Result;
+            }
+            catch (Exception e)
+            {
+                exception = e;
+            }
+            finally
+            {
+                var httpStatusIs403 = exception?.InnerException?.Message?.Contains("403") ?? false;
+
+                Assert.IsTrue(httpStatusIs403, $"Wrong result when asking for non-existing Dataset");
+            }
+        }
+
+        [TestMethod]
+        public void TestUpdateFeatures()
+        {
+            var tempFile = LockAndSaveFeatureByLokalId();
+
+            var wfsReplaceFile = Wfs.CreateReplaceWrappingForFeatures(tempFile, Ar5FeatureLokalId);
+
+            using (var featureStream = File.OpenRead(wfsReplaceFile))
+            {
+                var result = Client.UpdateDatasetFeaturesAsync(DatasetId, GetLocking(), featureStream).Result;
+
+                Assert.IsTrue(result.Features_replaced > 0, "No features updated");
+            };
+            
+            Client.DeleteDatasetLocksAsync(DatasetId, GetLocking());
+        }
+
+        [TestMethod]
+        public string TestGetFeaturesWithBbox()
+        {
+            var fileResponse = Client.GetDatasetFeaturesAsync(DatasetId, null, GetExampleBbox(), null).Result;
+
+            return WriteStreamToDisk(fileResponse);
+        }
+
+        private static string WriteStreamToDisk(FileResponse fileResponse)
+        {
+            var tempFile = Path.GetTempFileName();
+
+            using (var fileStream = File.OpenWrite(tempFile))
+            {
+                fileResponse.Stream.CopyTo(fileStream);
+            }
+
+            Assert.IsTrue(File.Exists(tempFile), "No features saved to disk");
+
+            var fileInfo = new FileInfo(tempFile);
+
+            Assert.IsTrue(fileInfo.Length > 0, "Empty file saved to disk");
+            
+            return tempFile;
         }
 
         //[TestMethod]
-        //public void TestGetDatasetWithWrongId()
+        //public void TestGetLockedFeaturesByLokalId()
         //{
-        //    var dataset = Client.GetDatasetMetadataAsync(WrongDatasetId).Result;
-        //    Assert.IsTrue(dataset.Id != DatasetId, $"Dataset found with wrong id {DatasetId}");
+        //    LockAndSaveFeatureByLokalId();
+
+        //    Client.DeleteDatasetLocksAsync(DatasetId, GetLocking());
+
+        //    Client.GetDatasetLocksAsync(DatasetId, GetLocking());
+
         //}
 
-        //[TestMethod]
-        //public void TestGetFeatures()
-        //{
-        //    var features = Client.GetDataAsync(DatasetId,Lock.FromJson(""),null,null).Result;
-        //}
+        private string LockAndSaveFeatureByLokalId()
+        {
+            var fileResponse = Client.GetDatasetFeaturesAsync(DatasetId, GetLocking(), null, GetExampleQuery()).Result;
+
+            return WriteStreamToDisk(fileResponse);
+        }
+
+        [TestMethod]
+        public string TestGetFeaturesByLokalId()
+        {
+            var fileResponse = Client.GetDatasetFeaturesAsync(DatasetId, null, null, GetExampleQuery()).Result;
+
+            return WriteStreamToDisk(fileResponse);
+        }
+
+        private string GetExampleQuery()
+        {
+            return $"eq(*/identifikasjon/lokalid,{Ar5FeatureLokalId})";
+        }
+
+        private BoundingBox GetExampleBbox()
+        {
+            var ll1 = 365600;
+            var ll2 = 7217500;
+            var ur1 = 366100;
+            var ur2 = 7217850;
+
+            return new BoundingBox { Ll = new List<double> { ll1,  ll2 }, Ur = new List<double> { ur1, ur2 } };
+        }
+
+        private Stream GetExampleFeatureStream(string fileName)
+        {
+            Assert.IsTrue(File.Exists(fileName), $"Example feature not found at {fileName}");
+
+            return new StreamReader(fileName).BaseStream;
+        }
+
+        private static Locking GetLocking()
+        {
+            return new Locking { Type = LockingType.User_lock };
+        }
     }
 }
---
+++ examples/dotnet/SFKB_clientTests/Wfs.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace SFKB_clientTests
+{
+    internal class Wfs
+    {
+        private static readonly XNamespace gmlNamespace = "http://www.opengis.net/gml/3.2";
+        private static readonly XNamespace wfsNamespace = "http://www.opengis.net/wfs/2.0";
+        private static readonly XNamespace fesNamespace = "http://www.opengis.net/fes/2.0";
+        private static readonly XNamespace xsiNamespace = "http://www.w3.org/2001/XMLSchema-instance";
+        private static readonly string appPrefix = "app";
+        private static readonly XNamespace chlogfNamespace = "http://skjema.geonorge.no/standard/geosynkronisering/1.1/endringslogg";
+        private static XNamespace activeNamespace;
+        private static string activeSchemaLocation;
+        private static readonly string chlogfSchemaLocation = "http://skjema.geonorge.no/standard/geosynkronisering/1.1/endringslogg http://skjema.geonorge.no/standard/geosynkronisering/1.1/endringslogg/changelogfile.xsd";
+
+        internal static string CreateReplaceWrappingForFeatures(string tempFile, string ar5FeatureLokalId)
+        {
+            var newTempFile = Path.GetTempFileName();
+
+            using (var featureStream = File.OpenRead(tempFile))
+            {
+                var featureXml = XElement.Load(featureStream);
+
+                var replaceXml = GetReplaceXml(featureXml, ar5FeatureLokalId);
+
+                replaceXml.Save(newTempFile);
+            }
+
+            return newTempFile;
+        }
+
+        private static XElement GetReplaceXml(XElement featureXml, string ar5FeatureLokalId)
+        {          
+            activeSchemaLocation = featureXml.Attribute(xsiNamespace + "schemaLocation").Value + " " + chlogfSchemaLocation;
+
+            activeNamespace = featureXml.Attribute(XNamespace.Xmlns + appPrefix).Value;
+
+            var changeLogElement = GetChangeLogElement();
+
+            var transactionElement = GetTransactionElement();
+
+            var featureMembers = featureXml.Descendants(gmlNamespace + "featureMember");
+
+            var activeFeature = featureMembers.FirstOrDefault( f => f.Descendants(activeNamespace + "lokalId").FirstOrDefault().Value == ar5FeatureLokalId);
+
+            var gmlId = activeFeature.Descendants().Where(d => d.Attribute(gmlNamespace + "id") != null && d.Attribute(gmlNamespace + "id").Value.StartsWith("QMS_")).FirstOrDefault();
+
+            var activeGeometry = gmlId.Attribute(gmlNamespace + "id").Value.Split("_")[2];
+
+            foreach (var featureMember in featureMembers)
+            {
+                var lokalId = GetLokalId(featureMember);
+
+                if (lokalId != ar5FeatureLokalId)
+                {
+                    var currentGmlIds = featureMember.Descendants().Where(d => d.Attribute(gmlNamespace + "id") != null && d.Attribute(gmlNamespace + "id").Value.StartsWith("QMS_"));
+
+                    if(!currentGmlIds.Any(id => id.Attribute(gmlNamespace + "id").Value.Contains(activeGeometry))) 
+                        continue;
+                }
+
+                string xpathExpressionLokalidFilter = GetLokalIdXpath();
+
+                var replaceElement = new XElement(wfsNamespace + "Replace");
+
+                replaceElement.Add(featureMember.FirstNode);
+
+                replaceElement.Add(GetFilter(lokalId, xpathExpressionLokalidFilter));
+
+                transactionElement.Add(replaceElement);
+            }
+            
+            changeLogElement.Add(transactionElement);
+
+            var count = featureMembers.Count();
+
+            changeLogElement.Add(
+                new XAttribute("startIndex", 1),
+                new XAttribute("endIndex", 1),
+                new XAttribute("numberMatched", count),
+                new XAttribute("numberReturned", count));
+                       
+            return changeLogElement;
+        }
+
+        private static XElement GetChangeLogElement()
+        {
+            return new XElement(chlogfNamespace + "TransactionCollection",
+                new XAttribute(XNamespace.Xmlns + "chlogf", chlogfNamespace.NamespaceName),
+                new XAttribute(XNamespace.Xmlns + "xsi", xsiNamespace.NamespaceName),
+                new XAttribute(XNamespace.Xmlns + appPrefix, activeNamespace.NamespaceName),
+                new XAttribute(XNamespace.Xmlns + "wfs", wfsNamespace.NamespaceName),
+                new XAttribute(XNamespace.Xmlns + "gml", gmlNamespace.NamespaceName),
+                new XAttribute(XNamespace.Xmlns + "fes", fesNamespace.NamespaceName),
+                new XAttribute(xsiNamespace + "schemaLocation", activeSchemaLocation),                
+                new XAttribute("timeStamp", DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.ffzzz"))
+                
+                );
+        }
+
+        private static XElement GetFilter(string lokalId, string xpathExpressionLokalidFilter)
+        {
+            return new XElement(fesNamespace + "Filter",
+                                new XElement(fesNamespace + "PropertyIsEqualTo",
+                                    new XElement(fesNamespace + "ValueReference", xpathExpressionLokalidFilter),
+                                    new XElement(fesNamespace + "Literal", lokalId)
+                                    )
+                                );
+        }
+
+        private static XElement GetTransactionElement()
+        {
+            return new XElement(chlogfNamespace + "transactions",
+                new XAttribute("version", "2.0.0"),
+                new XAttribute("service", "WFS"));
+        }
+
+        private static string GetLokalIdXpath()
+        {
+            return $"{appPrefix}:identifikasjon/{appPrefix}:Identifikasjon/{appPrefix}:lokalId";
+        }
+
+        private static string GetLokalId(XElement feature)
+        {
+            return feature.Descendants(activeNamespace + "lokalId").Nodes().FirstOrDefault().ToString();
+        }
+    }
+}
